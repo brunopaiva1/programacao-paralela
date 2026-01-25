b5049bf2c6a50a843adcf50d00ebb8cbe964baba
brunopaiva1 <brunopaivamaster10@gmail.com>
2026-01-24 23:25:28 -0300

    add cod paralelo com a utilização da soma em progressão aritmética de Gauss

--- pcd1/openmp/Dependency_of_datas/dependence.cpp
+++ pcd1/openmp/Dependency_of_datas/dependence.cpp
@@ -30,6 +30,21 @@ int main(){
     double end_serial = omp_get_wtime();
     std::cout << "Serial execution time: " << (end_serial - start_serial) << " seconds" << std::endl;
 
+    // trecho de código paralelo sem dependência de dados
+
+    omp_set_num_threads(8);
+
+    double start_parallel = omp_get_wtime();
+    a_parallel[0] = 0;
+
+    #pragma omp parallel for
+    for (long long i = 1; i < n; i++) {
+        a_parallel[i] = (i * (i + 1)) / 2; // Progressão aritmética de Gauss
+    }
+
+    double end_parallel = omp_get_wtime();
+    std::cout << "Parallel execution time: " << (end_parallel - start_parallel) << " seconds" << std::endl;
+
     return 0;
 
 }
