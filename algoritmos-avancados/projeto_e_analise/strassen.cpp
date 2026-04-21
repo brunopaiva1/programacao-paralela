56d41afe9992ddd3ea228fef35ba801a6f7787e4
brunopaiva1 <brunopaivamaster10@gmail.com>
2026-04-21 00:57:01 -0300

    add algoritmo de strassen

--- algoritmos-avancados/projeto_e_analise/strassen.cpp
+++ algoritmos-avancados/projeto_e_analise/strassen.cpp
@@ -39,3 +39,63 @@ Matrix conventional_multiply(const Matrix& A, const Matrix& B) {
     return C;
 }
 
+Matrix strassen(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    if (n == 1) {
+        return {{A[0][0] * B[0][0]}};
+    }
+    int newSize = n / 2;
+    Matrix A11(newSize, vector<int>(newSize));
+    Matrix A12(newSize, vector<int>(newSize));
+    Matrix A21(newSize, vector<int>(newSize));
+    Matrix A22(newSize, vector<int>(newSize));
+    Matrix B11(newSize, vector<int>(newSize));
+    Matrix B12(newSize, vector<int>(newSize));
+    Matrix B21(newSize, vector<int>(newSize));
+    Matrix B22(newSize, vector<int>(newSize));
+
+    for (int i = 0; i < newSize; i++) {
+        for (int j = 0; j < newSize; j++) {
+            A11[i][j] = A[i][j];
+            A12[i][j] = A[i][j + newSize];
+            A21[i][j] = A[i + newSize][j];
+            A22[i][j] = A[i + newSize][j + newSize];
+            B11[i][j] = B[i][j];
+            B12[i][j] = B[i][j + newSize];
+            B21[i][j] = B[i + newSize][j];
+            B22[i][j] = B[i + newSize][j + newSize];
+        }
+    }
+
+    Matrix M1 = strassen(add(A11, A22), add(B11, B22));
+    Matrix M2 = strassen(add(A21, A22), B11);
+    Matrix M3 = strassen(A11, subtract(B12, B22));
+    Matrix M4 = strassen(A22, subtract(B21, B11));
+    Matrix M5 = strassen(add(A11, A12), B22);
+    Matrix M6 = strassen(subtract(A21, A11), add(B11, B12));
+    Matrix M7 = strassen(subtract(A12, A22), add(B21, B22));
+
+    Matrix C(n, vector<int>(n));
+    for (int i = 0; i < newSize; i++) {
+        for (int j = 0; j < newSize; j++) {
+            C[i][j] = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
+        }
+    }
+    for (int i = 0; i < newSize; i++) {
+        for (int j = 0; j < newSize; j++) {
+            C[i][j + newSize] = M3[i][j] + M5[i][j];
+        }
+    }
+    for (int i = 0; i < newSize; i++) {
+        for (int j = 0; j < newSize; j++) {
+            C[i + newSize][j] = M2[i][j] + M4[i][j];
+        }
+    }
+    for (int i = 0; i < newSize; i++) {
+        for (int j = 0; j < newSize; j++) {
+            C[i + newSize][j + newSize] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+        }
+    }
+    return C;
+}
+
