3584bc0ceef686603661a4e4636e1f6419e1b174
brunopaiva1 <brunopaivamaster10@gmail.com>
2026-04-21 00:53:55 -0300

    add operações convencionais da multiplicação de matrizes

---
+++ algoritmos-avancados/projeto_e_analise/strassen.cpp
@@ -0,0 +1,41 @@
+// 1. Mostre numericamente com suas implementações dos algoritmos de
+// multiplicação de matrizes que o algoritmo de Strassen é mais rápido que o
+// algoritmo convencional.
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <fstream>
+
+using namespace std;
+
+using Matrix = vector<vector<int>>;
+
+Matrix add(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] + B[i][j];
+    return C;
+}
+
+Matrix subtract(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] - B[i][j];
+    return C;
+}
+
+Matrix conventional_multiply(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n, 0));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            for (int k = 0; k < n; k++)
+                C[i][j] += A[i][k] * B[k][j];
+    return C;
+}
+
